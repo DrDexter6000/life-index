eaf98eb38e5f5d68286a6b6fd172b4df49796691
Dr.Dexter <superjay6000@gmail.com>
2026-04-09 12:25:22 +0100

    ﻿chore: remove archived web gui leftovers
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- .archive/web/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Web GUI route blueprints."""
-
-from . import dashboard
-from . import edit
-from . import journal
-from . import search
-from . import write
-
-__all__ = ["dashboard", "edit", "journal", "search", "write"]
